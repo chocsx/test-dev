4ef4bf8082c29c6e8871ea0a07277567cfac55cf
Mateus Sobrinho <mateus.sobrinho@i9xp.com.br>
2019-09-18 00:16:09 -0300

    correcao caminho readme.md

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+use Slim\Factory\AppFactory;
+use Slim\Middleware\ErrorMiddleware;
+
+require 'vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$errorMiddleware = new ErrorMiddleware(
+    $app->getCallableResolver(),
+    $app->getResponseFactory(),
+    true,
+    false,
+    false
+);
+
+$app->add($errorMiddleware);
+
+$app->run();
